0bc68ce2f772bc79e710cc6cd51d027d9803661d
Patrick Ferris <patrick@sirref.org>
2024-07-23 00:06:19 +0100

    Eio Irmin + escaping effects

--- bin/main.ml
+++ bin/main.ml
@@ -13,25 +13,27 @@ end
 
 module Store = Irmin_fs_unix.KV.Make (Irmin.Contents.String)
 module Term = Asai.Tty.Make (Reporter.Message)
-open Lwt.Syntax
 
-let test () =
-  let config = Irmin_fs.config "store" in
-  let* repo = Store.Repo.v config in
-  let* main = Store.main repo in
+let test ~path ~clock =
+  let config = Irmin_fs_unix.conf ~path ~clock in
+  let repo = Store.Repo.v config in
+  let main = Store.main repo in
   let info () = Store.Info.v 0L in
   let key = "Hello" in
-  let* _ =
-    match%lwt Store.set main [ key ] ~info "world!" with
+  Eio.Switch.run @@ fun sw ->
+  Eio.Fiber.fork ~sw (fun () -> Reporter.fatalf Hello "I now fail with Unhandled!");
+  let () =
+    match Store.set main [ key ] ~info "world!" with
     | _ ->
         Reporter.fatalf Hello
           {|"I won't be reported by Asai, instead you'll see "Fatal error: exception Unhandled asai effect/exception; use Reporter.run"|}
   in
-  let* v = Store.get main [ key ] in
-  print_endline v |> Lwt.return
+  let v = Store.get main [ key ] in
+  print_endline v
 
 let () =
   Eio_main.run @@ fun env ->
   Reporter.run ~emit:Term.display ~fatal:Term.display @@ fun () ->
-  Lwt_eio.with_event_loop ~clock:env#clock @@ fun _ ->
-  Lwt_eio.run_lwt @@ fun () -> test ()
+  let path = Eio.Path.(env#cwd / "store") in
+  let clock = Eio.Stdenv.clock env in
+  test ~path ~clock
