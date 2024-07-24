14003508fd5f9318e9bd01224f8a16af349e6d46
Kento Okura <kentookura@gmail.com>
2024-07-24 16:08:20 +0200

    make code run, effects still unhandled

--- bin/main.ml
+++ bin/main.ml
@@ -14,26 +14,28 @@ end
 module Store = Irmin_fs_unix.KV.Make (Irmin.Contents.String)
 module Term = Asai.Tty.Make (Reporter.Message)
 
-let test ~path ~clock =
-  let config = Irmin_fs_unix.conf ~path ~clock in
+let test () =
+  let config = Irmin_fs.config "store" in
   let repo = Store.Repo.v config in
   let main = Store.main repo in
   let info () = Store.Info.v 0L in
   let key = "Hello" in
-  Eio.Switch.run @@ fun sw ->
-  Eio.Fiber.fork ~sw (fun () -> Reporter.fatalf Hello "I now fail with Unhandled!");
+  (* Eio.Switch.run @@ fun sw -> *)
+  (* Eio.Fiber.fork ~sw (fun () -> *)
+  (*     Reporter.fatalf Hello "I now fail with Unhandled!"); *)
   let () =
     match Store.set main [ key ] ~info "world!" with
     | _ ->
         Reporter.fatalf Hello
-          {|"I won't be reported by Asai, instead you'll see "Fatal error: exception Unhandled asai effect/exception; use Reporter.run"|}
+          {|I won't be reported by Asai, instead you'll see "Fatal error: exception Unhandled asai effect/exception; use Reporter.run|}
   in
   let v = Store.get main [ key ] in
   print_endline v
 
 let () =
-  Eio_main.run @@ fun env ->
   Reporter.run ~emit:Term.display ~fatal:Term.display @@ fun () ->
+  Eio_main.run @@ fun env ->
   let path = Eio.Path.(env#cwd / "store") in
-  let clock = Eio.Stdenv.clock env in
-  test ~path ~clock
+  test ()
+(* let clock = Eio.Stdenv.clock env in *)
+(* test ~path ~clock *)
